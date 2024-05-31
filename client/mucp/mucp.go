074a5cf6580b03273aeae270f563274744941cf1
mason <mason@udmire.cn>
2024-05-31 07:09:03 +0000

    add http & mucp client

---
+++ client/mucp/mucp.go
@@ -0,0 +1,16 @@
+// Package mucp provides an mucp client
+package mucp
+
+import (
+	"github.com/smart-echo/micro/client"
+	"github.com/smart-echo/micro/util/cmd"
+)
+
+func init() {
+	cmd.DefaultClients["mucp"] = NewClient
+}
+
+// NewClient returns a new micro client interface.
+func NewClient(opts ...client.Option) client.Client {
+	return client.NewClient(opts...)
+}
